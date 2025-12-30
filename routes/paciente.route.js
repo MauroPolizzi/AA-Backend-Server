cc64ebb5ead3796c9a3dc8f6e5e994948a39368c
MauroPolizzi <mauropolizzi2@gmail.com>
2025-12-30 16:17:43 -0300

    validation in field required for paciente.route

--- routes/paciente.route.js
+++ routes/paciente.route.js
@@ -1,16 +1,40 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { Genero, TipoSangre, TipoDocumento } = require('../models/enums');
 const { getPacientes, postPaciente, getPacienteById } = require('../controllers/paciente.controller');
+const { validarCampos } = require('../middlewares/validator-campos');
 
 const pacienteRouter = Router();
 
-// Get 
+// Get
 pacienteRouter.get( '/', getPacientes );
 
 // Get By Id
 pacienteRouter.get('/:guid', getPacienteById );
 
 // Post
-pacienteRouter.post( '/', postPaciente );
+pacienteRouter.post( '/', 
+    [
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('apellido', 'El apellido es obligatorio').not().isEmpty(),
+        check('tipoDocumento', 'El tipo de documento es obligatorio').not().isEmpty(),
+        check('tipoDocumento', 'El tipo de documento no es válido').isIn(Object.values(TipoDocumento)),
+        check('numeroDocumento', 'El número de documento es obligatorio').not().isEmpty(),
+        check('numeroDocumento', 'El número de documento debe ser numérico').isNumeric(),
+        check('fechaNacimiento', 'La fecha de nacimiento es obligatoria').not().isEmpty(),
+        check('fechaNacimiento', 'La fecha de nacimiento debe ser una fecha válida').isISO8601(),
+        check('genero', 'El género es obligatorio').not().isEmpty(),
+        check('genero', 'El género no es válido').isIn(Object.values(Genero)),
+        check('tipoSangre', 'El tipo de sangre es obligatorio').not().isEmpty(),
+        check('tipoSangre', 'El tipo de sangre no es válido').isIn(Object.values(TipoSangre)),
+        check('email', 'El email es obligatorio').not().isEmpty(),
+        check('email', 'El email no es válido').isEmail(),
+        check('direccion', 'La dirección es obligatoria').not().isEmpty(),
+        check('usuarioId', 'El ID de usuario es obligatorio').not().isEmpty(),
+        check('usuarioId', 'El ID de usuario debe ser un ObjectId válido').isMongoId(),
+        validarCampos
+    ], 
+    postPaciente );
 
 module.exports = {
     pacienteRouter
